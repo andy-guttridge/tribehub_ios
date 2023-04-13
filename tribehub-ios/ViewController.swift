dc6d07b85504d2261f6ad388c3a6310e51d05cea
Andy Guttridge <andyguttridge123@gmail.com>
2023-04-13 11:39:44 +0100

    Feature: add basic UI for testing user authentication

--- tribehub-ios/ViewController.swift
+++ tribehub-ios/ViewController.swift
@@ -10,79 +10,97 @@ import Alamofire
 
 class ViewController: UIViewController {
     
+    var credentials: NSDictionary?
+    var session: Session?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Test logging in to the REST API and retrieving some data
-        Task.init {
-            do {
-                await self.testApi()
-            }
-        }
-    }
-    
-    func testApi() async {
-        
-        // Retrieve test login credentials from app bundle
-        var credentials: NSDictionary?
+        // Retrieve credentials
         if let path = Bundle.main.path(forResource: "test_credentials", ofType: "plist") {
             credentials = NSDictionary(contentsOfFile: path)
         }
         
         // Create Alamofire session
-        let session = Session.default
+        self.session = Session.default
+    }
+    
+//    func testApi() async {
+//
+//
+//
+//
+//        // Create userProfileRequest object for retrieving the user profile and use to retrieve a profile
+////        let userProfileRequest = APIRequest(resource: UserResource(), session: session)
+////        do {
+////            if let response = try await userProfileRequest.fetchData() {
+////                print(response)
+////            }
+////        } catch {
+////            print(error)
+////        }
+//
+//
+//
+//
+//    }
+    
+    @IBAction func doLogin(_ sender: Any) {
+        guard let session = self.session else {
+            print("No Alamofire session in doLogin")
+            return
+        }
         
         // Create loginRequest onject for logging in and use the postData method to login with user credentials.
         // We get a LoginResponse object with the access token, refresh token and user profile
         let loginRequest = APIRequest(resource: LoginResource(), session: session)
-        do {
-            if let response = try await loginRequest.postData(payload: credentials as? Dictionary<String, Any>) {
-                print(response)
+        Task.init {
+            do {
+                if let response = try await loginRequest.postData(payload: credentials as? Dictionary<String, Any>) {
+                    print(response)
+                }
+            } catch {
+                print(error)
             }
-        } catch {
-            print(error)
+        }
+    }
+    
+    @IBAction func fetchTribe(_ sender: Any) {
+        guard let session = self.session else {
+            print("No Alamofire session in fetchTribe")
+            return
         }
         
-        // Create userProfileRequest object for retrieving the user profile and use to retrieve a profile
-        let userProfileRequest = APIRequest(resource: UserResource(), session: session)
-        do {
-            if let response = try await userProfileRequest.fetchData() {
-                print(response)
+        Task.init {
+            // Create tribeRequest object for retrieving user's tribe details and use to retrieve tribe members
+            let tribeRequest = APIRequest(resource: TribeResource(), session: session)
+            do {
+                if let response = try await tribeRequest.fetchData() {
+                    print(response)
+                }
+            } catch {
+                print(error)
             }
-        } catch {
-            print(error)
         }
-        
-        // Create tribeRequest object for retrieving user's tribe details and use to retrieve tribe members
-        let tribeRequest = APIRequest(resource: TribeResource(), session: session)
-        do {
-            if let response = try await tribeRequest.fetchData() {
-                print(response)
-            }
-        } catch {
-            print(error)
+    }
+    
+    @IBAction func doLogout(_ sender: Any) {
+        guard let session = self.session else {
+            print("No Alamofire session in doLogout")
+            return
         }
         
         // Create a logoutRequest object for logging out and use to logout. We get an empty LogInResponse object back.
         let logoutRequest = APIRequest(resource: LogoutResource(), session: session)
-        do {
-            if let response = try await logoutRequest.postData(payload: nil) {
-                print(response)
+        Task.init {
+            do {
+                if let response = try await logoutRequest.postData(payload: nil) {
+                    print(response)
+                }
+            } catch {
+                print(error)
             }
-        } catch {
-            print(error)
         }
-        
-        
-        // Log in to the API, retrieve user's tribe members and log out
-        //        let baseUrl = "https://tribehub-drf.herokuapp.com"
-        //        AF.request("\(baseUrl)/dj-rest-auth/login/", method: .post, parameters: credentials as! Parameters).responseJSON {response in
-        //            debugPrint(response.value)
-        //            AF.request("\(baseUrl)/tribe/").responseJSON {response in
-        //                debugPrint(response.value)
-        //                AF.request("\(baseUrl)/dj-rest-auth/logout/", method: .post).responseJSON {response in debugPrint(response)}
-        //            }
-        //        }
     }
 }
 
