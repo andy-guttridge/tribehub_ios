19e55fed57b301afa17c37f7bd0f47bd47a14392
Andy Guttridge <andyguttridge123@gmail.com>
2023-04-23 18:14:35 +0100

    Feature: add tribe members table view to account tab

--- tribehub-ios/AccountViewController.swift
+++ tribehub-ios/AccountViewController.swift
@@ -7,16 +7,32 @@
 
 import UIKit
 
-class AccountViewController: UIViewController {
+class AccountViewController: UIViewController, UITableViewDataSource {
+    
     weak var userModelController: UserModelController?
     weak var tribeModelController: TribeModelController?
 
     @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var tribeTableView: UITableView!
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         self.profileImageView.image = userModelController?.user?.profileImage
         self.profileImageView.makeRounded()
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.tribeModelController?.tribe?.tribeMembers.count ?? 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tribeTableView.dequeueReusableCell(withIdentifier: "tribeMemberCell", for: indexPath)
+        var contentConfiguration = cell.defaultContentConfiguration()
+        contentConfiguration.image = self.tribeModelController?.tribe?.tribeMembers[indexPath.row].profileImage
+        contentConfiguration.imageProperties.maximumSize = CGSize(width: 32, height: 32)
+        contentConfiguration.text = self.tribeModelController?.tribe?.tribeMembers[indexPath.row].displayName
+        cell.contentConfiguration = contentConfiguration
+        return cell
     }
 
   
