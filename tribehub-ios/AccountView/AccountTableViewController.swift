dfd98f953e15aa062a53c5c074573ecec35702ae
Andy Guttridge <andyguttridge123@gmail.com>
2023-04-27 18:23:09 +0100

    Feature: add logout functionality to Manage Account table and ensure Manage Tribe is reset after user logs out, to ensure only tribe admin users can select this option

--- tribehub-ios/AccountView/AccountTableViewController.swift
+++ tribehub-ios/AccountView/AccountTableViewController.swift
@@ -16,23 +16,48 @@ class AccountTableViewController: UITableViewController {
     }
     
     override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        self.checkTribeAdminStatus()
         
-        // Enable manage tribe cell if user is tribe admin
-        if self.userModelController?.user?.isAdmin == true {
-            let manageTribeCell = self.tableView(self.tableView, cellForRowAt: IndexPath(row: 0, section: 0))
-            manageTribeCell.isUserInteractionEnabled = true
-            manageTribeCell.textLabel?.isEnabled = true
-            manageTribeCell.imageView?.tintColor = .systemBlue
-        }
     }
-
+    
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         if indexPath.row == 3 {
             print("Did select delete account")
         }
         if indexPath.row == 4 {
-            print("Did select logout")
+            // User selected logout
+            guard let userModelController = self.userModelController else {
+                return
+            }
+            
+            Task.init {
+                do {
+                    _ = try await userModelController.doLogout()
+                    self.checkTribeAdminStatus()
+                } catch {
+                    let alert = UIAlertController(title: "Logout Error", message:"There was an issue logging out.", preferredStyle: .alert)
+                    alert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "Default action"), style: .default, handler: {alertAction in alert.dismiss(animated: true)}))
+                    self.present(alert, animated: true)
+                }
+            }
         }
     }
-
+    
+    /// Ensure manage tribe cell is only selectable if user is tribe admin,
+    /// otherwise grey out and make unselectable
+    func checkTribeAdminStatus() -> Void {
+        if self.userModelController?.user?.isAdmin == true {
+            let manageTribeCell = self.tableView(self.tableView, cellForRowAt: IndexPath(row: 0, section: 0))
+            manageTribeCell.isUserInteractionEnabled = true
+            manageTribeCell.textLabel?.isEnabled = true
+            manageTribeCell.imageView?.tintColor = .systemBlue
+        } else {
+            let manageTribeCell = self.tableView(self.tableView, cellForRowAt: IndexPath(row: 0, section: 0))
+            manageTribeCell.isUserInteractionEnabled = false
+            manageTribeCell.textLabel?.isEnabled = false
+            manageTribeCell.imageView?.tintColor = .gray
+        }
+    }
+    
 }
