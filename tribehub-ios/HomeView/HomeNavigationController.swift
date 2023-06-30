883cfbe2840f86b4eb99fe0092f41b922bfb9b5e
Andy Guttridge <andyguttridge123@gmail.com>
2023-06-30 18:16:22 +0100

    Feature: add searchController to HomeNavigationController and EventSearchResultsTableViewController, ready to implement events search functionality

--- tribehub-ios/HomeView/HomeNavigationController.swift
+++ tribehub-ios/HomeView/HomeNavigationController.swift
@@ -11,6 +11,7 @@ class HomeNavigationController: UINavigationController {
     weak var eventsModelController: EventsModelController?
     weak var userModelController: UserModelController?
     weak var tribeModelController: TribeModelController?
+    weak var homeViewController: HomeViewController?
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -31,7 +32,22 @@ private extension HomeNavigationController {
             homeViewController.eventsModelController = eventsModelController
             homeViewController.userModelController = userModelController
             homeViewController.tribeModelController = tribeModelController
+            self.homeViewController = homeViewController
             
+            // Configure searchController
+            // Get a reference to the tableView for presenting search results and use it to instantiate a searchController
+            if let eventSearchResultsTableView = storyboard?.instantiateViewController(withIdentifier: "EventSearchTableViewController") as? EventSearchResultsTableViewController {
+                navigationBar.topItem?.searchController = UISearchController.init(searchResultsController: eventSearchResultsTableView)
+                navigationBar.topItem?.searchController?.searchBar.delegate = eventSearchResultsTableView
+                eventSearchResultsTableView.tribeModelController = tribeModelController
+                eventSearchResultsTableView.eventsModelController = eventsModelController
+            }
+            navigationBar.topItem?.searchController?.delegate = self
+            navigationBar.topItem?.preferredSearchBarPlacement = .inline
+            navigationBar.topItem?.searchController?.scopeBarActivation = .onSearchActivation
+            navigationBar.topItem?.searchController?.searchBar.scopeButtonTitles = ["Subject", "Tribe", "Category", "From", "To"]
+            
+            // Configure navigation bar
             let navigationBarAppearance = UINavigationBarAppearance()
             navigationBarAppearance.backgroundColor = UIColor(named: "THBackground")
             navigationBar.tintColor = UIColor(named: "THIcons")
@@ -41,3 +57,10 @@ private extension HomeNavigationController {
         }
     }
  }
+
+// MARK: UISearchControllerDelegate
+extension HomeNavigationController: UISearchControllerDelegate {
+    func didDismissSearchController(_ searchController: UISearchController) {
+        print("SearchController was dismissed")
+    }
+}
